aeb9e0327d6f55fac3653ddf411d5ab67a9f1962
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-06-13 15:30:48 +0600

    up (#1)
    * Configuration to disable internal stmt cache
    
    This enables usage with pgBouncer's transaction mode. Typically when
    using the transaction mode, a client gets a new connection from
    pgBouncer for every new transaction. It's quite useful and allows one to
    use prepared statements in this mode. The workflow goes:
    
    ```sql
    -- start a new transaction
    BEGIN
    -- deallocate all stored statements from the server to prevent
    -- collisions
    DEALLOCATE ALL
    -- run the queries here
    -- ..
    -- ..
    COMMIT -- or ROLLBACK
    ```
    
    Now in a case where the query uses custom types such as enums, what
    tokio-postgres does is it fetches the type info for the given type,
    stores the info to the cache and also caches the statements for
    fetching the info to the client. Now when we have two tables with
    different custom types in both of them, we can imagine the following
    workflow:
    
    ```rust
    // first query
    client.simple_query("BEGIN")?;
    client.simple_query("DEALLOCATE ALL")?;
    let stmt = client.prepare("SELECT \"public\".\"User\".\"id\", \"public\".\"User\".\"userType\" FROM \"public\".\"User\" WHERE 1=1 OFFSET $1")?;
    dbg!(client.query(&stmt, &[&0i64])?);
    client.simple_query("COMMIT")?;
    
    // second query
    client.simple_query("BEGIN")?;
    client.simple_query("DEALLOCATE ALL")?;
    let stmt = client.prepare("SELECT \"public\".\"Work\".\"id\", \"public\".\"Work\".\"workType\" FROM \"public\".\"Work\" WHERE 1=1 OFFSET $1")?;
    dbg!(client.query(&stmt, &[&0i64])?);
    client.simple_query("COMMIT")?;
    ```
    
    The `userType` and `workType` are both enums, and the preparing of the
    second query will give an error `prepared statement "s1" does not
    exist`, where `s1` is the query to the `pg_catalog` for the type info.
    
    The change here gives an extra flag for the client to disable caching
    of statements.
    
    * Allow vendoring openssl
    
    * Back to mainline tokio-native-tls
    
    * feat: enable setting search_path
    
    * fix(tokio-postgres): update socket2 to fix tcp keep alive "with_interval"
    
    * chore: bump `socket2` to `0.5.3`
    
    * perf: avoid fetching enum variants
    
    * remove dbg stmts
    
    ---------
    
    Co-authored-by: Julius de Bruijn <julius+github@nauk.io>
    Co-authored-by: Flavian Desverne <flavian.dsv@gmail.com>
    Co-authored-by: jkomyno <skiabo97@gmail.com>
    Co-authored-by: Alberto Schiabel <jkomyno@users.noreply.github.com>

--- postgres-types/src/lib.rs
+++ postgres-types/src/lib.rs
@@ -367,7 +367,7 @@ pub enum Kind {
     /// A simple type like `VARCHAR` or `INTEGER`.
     Simple,
     /// An enumerated type along with its variants.
-    Enum(Vec<String>),
+    Enum,
     /// A pseudo-type.
     Pseudo,
     /// An array type along with the type of its elements.
